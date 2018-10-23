0818dc557ecda1d4b40904a43b0bc2dbfddc1e88
Elljay <32459954+hayomide@users.noreply.github.com>
2018-10-23 13:38:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="daisy">
     <div class="daisy__main">
         <input class="daisy__main__inpt" type="text" placeholder="Am Daisy. Here to assist you on any of your web activities" value="" />
-        <a class="daisy__main__mic" href="#" title="Click this to use mic"><img class="daisy__main__mic__icon" src="assets/images/mic_20x29_cmp.png" alt="Mic" /></a>
+        <a class="daisy__main__mic" href="#" title="Click this to use mic"><img class="daisy__main__mic__icon" src="assets/images/mic.png" alt="Mic" /></a>
         <div>
             <div class="daisy__main__note"></div>
             <a class="daisy__main__lnk lnch-popup" href="#daisy__explnd">See a list of thing I can do for you</a>
