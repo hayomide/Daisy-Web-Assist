c76197f3033055cdc7dbe929fe245a22f42319fb
Elljay <32459954+hayomide@users.noreply.github.com>
2018-10-23 13:16:50 +0100

    Delete test.php

--- test.php
+++
@@ -1,3 +0,0 @@
-<?php $img=imagecreatefrompng('assets/images/mic_20x29.png');
-if(imagepng($img,'assets/images/mic_20x29_cmp.png',9)) echo 'Compressed!'; else echo 'Not compressed!';
-imagedestroy($img);
