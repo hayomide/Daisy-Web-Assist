bc68cbf7b1d0903d80b6f40ae28a01e9bdd3d604
Elljay <32459954+hayomide@users.noreply.github.com>
2018-10-23 11:41:14 +0100

    Update lookup.php

--- lookup.php
+++ lookup.php
@@ -1,6 +1,6 @@
 <?php
 include 'funcs.php';
-if(!isset($_GET['origin'])||empty($_GET['origin'])||!isset($_GET['req_url'])||empty($_GET['req_url'])) die('Host or Req Url or Query not set');
-$origin=urldecode($_GET['origin']); $req_url=urldecode($_GET['req_url']); $query=isset($_GET['query'])&&!empty($_GET['query'])?$_GET['query']:'';
-$url=preg_replace('/\s+/','+',$origin.$req_url.$query);
+if(!isset($_GET['url'])||empty($_GET['url'])) die('Url not set');
+$url=urldecode($_GET['url']);
+$url=preg_replace('/\s+/','+',$url);
 echo lookup($url,false,'https://ejaworld.com/Daisy/',true);
